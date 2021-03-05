a8d6ec17eabc1458fb5e37ea08633fcd400b9225
aniledev <elinamcgill@gmail.com>
2021-03-05 17:15:28 -0500

    CHORE: update app rendering

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Root from "./root/Root.js";
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
--- src/root/Root.js
+++ src/root/Root.js
@@ -1,21 +1,8 @@
-import React from "react";
-import "../root/Root.css";
+import React, { Component } from 'react'
 
-function Root() {
-  return (
-    <div className="Root">
-      <h1>FIFO Pet Adoption</h1>
-      <p>Welcome to the FIFO Pet Adoption Site!</p>
-      <p>
-        Here at FIFO we believe that every pet deserves a chance at there
-        furever home. This vision inspired us to create one of the first and
-        premier FIFO pet adoption services. This means that the first cat or dog
-        to come to shelter must be adopted first. If you wish to join the
-        waiting list and take one of our adorable pets home soon, click the link
-        below.
-      </p>
-    </div>
-  );
-}
-
-export default Root;
+export default class Root extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>)}}
