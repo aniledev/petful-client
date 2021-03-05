841e4ad074589820954a720d87a80cd711a764d8
aniledev <elinamcgill@gmail.com>
2021-03-05 18:53:48 -0500

    FEAT: flesh out Root component JSX

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import Root from "./root/Root.js";
+import "./index.css";
+
 
 ReactDOM.render(
   <React.StrictMode>
--- src/root/Root.js
+++ src/root/Root.js
@@ -1,8 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "./Root.css";
 
-export default class Root extends Component {
+class Root extends Component {
   render() {
     return (
-      <div>
-        
-      </div>)}}
+      <div className="Root">
+        <div className="overlay">
+          <h1>FIFO Pet Adoption</h1>
+          <p>
+            Welcome to the FIFO Pet adoption website! Here at FIFO we believe
+            that each pet should have a chance at their furever home. This
+            vision inspired us to create the first and premier FIFO pet adopton
+            service. This means that the first pet to come to the shelter is the
+            first cat or dog that gets adopted.
+          </p>
+          <p>
+            To join the queue of adopters, click the buttton below. When it's
+            your chance to adopt, get ready to take your furbaby home!
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Root;
