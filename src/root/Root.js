9d2da62c44a588935e81bac0dd333eca330da7f8
aniledev <elinamcgill@gmail.com>
2021-03-05 16:49:56 -0500

    FEAT: add dummy text to landing page

--- src/root/Root.js
+++ src/root/Root.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import "../root/Root.css";
 
 function Root() {
-  return <div>
-    <h1>Petful</h1>
-  </div>
+  return (
+    <div className="Root">
+      <h1>FIFO Pet Adoption</h1>
+      <p>Welcome to the FIFO Pet Adoption Site!</p>
+      <p>
+        Here at FIFO we believe that every pet deserves a chance at there
+        furever home. This vision inspired us to create one of the first and
+        premier FIFO pet adoption services. This means that the first cat or dog
+        to come to shelter must be adopted first. If you wish to join the
+        waiting list and take one of our adorable pets home soon, click the link
+        below.
+      </p>
+    </div>
+  );
 }
 
-export default Root
+export default Root;
