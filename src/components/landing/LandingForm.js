36534791ea30ac0f0a67a6bf7b5ba6a7048c9fc7
aniledev <elinamcgill@gmail.com>
2021-03-12 15:14:00 -0500

    FEAT: intialize constructor for this.state.name

--- src/components/landing/LandingForm.js
+++ src/components/landing/LandingForm.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 
 class LandingForm extends Component {
+  // initialize state to hold controlled form components
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+    };
+  }
+
   render() {
     return (
       <div className="d-flex justify-content-center input mb-5 pb-5">
