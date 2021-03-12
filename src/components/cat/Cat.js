fbc158a578206823b69c2f50a5be6df240784281
aniledev <elinamcgill@gmail.com>
2021-03-12 14:25:11 -0500

    CHORE: add pseudocode for HTTP requests to the server

--- src/components/cat/Cat.js
+++ src/components/cat/Cat.js
@@ -13,12 +13,21 @@ export default class Cat extends Component {
     };
   }
 
+  componentDidMount() {
+    /* when the component is mounted, immediately preform a GET request to /api/cats 
+       to receive cat image and cat info */
+    // use chained promises to receive the data from the server
+    // use this.setState to update state with response data
+  }
+
   render() {
     return (
       <div class="col-md text-center">
         <CatHeading />
         <CatImage />
+        {/* once this.state.cats is updated, pass down description and imageUrl to CatImage component */}
         <CatInfo />
+        {/* once this.state.cats is updated, pass down age, breed, gender, name and story to CatImage component */}
         <div class="mb-5">
           <button type="button" class="btn adopt-button">
             Adopt Cat!
--- src/components/dog/Dog.js
+++ src/components/dog/Dog.js
@@ -5,6 +5,7 @@ import DogImage from "./DogImage";
 import DogInfo from "./DogInfo";
 
 export default class Dog extends Component {
+  // initialize state to hold fetch data for dogs
   constructor(props) {
     super(props);
     this.state = {
@@ -12,17 +13,20 @@ export default class Dog extends Component {
     };
   }
 
-    };
+  componentDidMount() {
+    /* when the component is mounted, immediately preform a GET request to /api/dogs 
+       to receive dog image and dog info */
+    // use chained promises to receive the data from the server
+    // use this.setState to update state with response data
   }
   render() {
     return (
       <div class="col-md text-center">
         <DogHeading />
-
+        {/* once this.state.dogs is updated, pass down description and imageUrl to dogImage component */}
         <DogImage />
-
+        {/* once this.state.dogs is updated, pass down age, breed, gender, name and story to dogImage component */}
         <DogInfo />
-
         <div class="mb-5">
           <button type="button" class="btn adopt-button">
             Adopt Dog!
