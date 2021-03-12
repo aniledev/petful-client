af7ed60f9932a21859d411c678a0b032a084d4a9
aniledev <elinamcgill@gmail.com>
2021-03-12 14:59:51 -0500

    CHORE: delete unused vars

--- src/components/adopt/AdoptSelection.js
+++ src/components/adopt/AdoptSelection.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import "../../styles/adopt-view.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCaretLeft, faCaretRight } from "@fortawesome/free-solid-svg-icons";
 import Cat from "../cat/Cat";
 import Dog from "../dog/Dog";
 
