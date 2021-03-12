7f92abd8ba46b5c3a10230246fc81cc4735503cb
aniledev <elinamcgill@gmail.com>
2021-03-12 13:37:42 -0500

    REFRACTOR: add queue(0) to generate names of people in line to adopt

--- src/components/adopt/AdoptQueue.js
+++ src/components/adopt/AdoptQueue.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
+import STORE from "../../dummy-store";
 
 export default class AdoptQueue extends Component {
   render() {
+    function queue() {
+      const adopters = STORE.people;
+      let queue = "";
+      for (let i = 0; i < adopters.length; i++) {
+        if (i < adopters.length - 1) {
+          queue += adopters[i] + ", ";
+        } else {
+          queue += adopters[i];
+        }
+      }
+      return queue;
+    }
+
     return (
       <div class="text-center adoption-queue mb-5">
-        <p>Next in line: Tyanna, Terana, Aisah, Treasure, Travis, Jocelyn</p>
+        <p>Next in line: {queue()}</p>
       </div>
     );
   }
