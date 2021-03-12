50270eab9adb04b135b28e17764746a005c3966d
aniledev <elinamcgill@gmail.com>
2021-03-12 14:42:57 -0500

    CHORE: update config variables

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-  PORT_URL: "https://fifo-pet-server.herokuapp.com/",
+  PORT_URL: "https://fifo-pet-server.herokuapp.com",
 };
