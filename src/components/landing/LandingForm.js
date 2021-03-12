18eae4f27b0793ec609a9c8e25d87c1ffbd04950
aniledev <elinamcgill@gmail.com>
2021-03-12 15:14:27 -0500

    CHORE: write pseudocode for form validation

--- src/components/landing/LandingForm.js
+++ src/components/landing/LandingForm.js
@@ -15,6 +15,13 @@ class LandingForm extends Component {
     // trigger the addNameToQueue method to send this.state.name to the server to add to queue
   }
 
+  // write method to validate the input box
+  validateName() {
+    // name cannot be empty upon submission
+    // name must contain at least 3 characters
+    // name cannot contain more than 100 characters
+    // use the validateName function to disable submission of the form
+  }
   render() {
     return (
       <div className="d-flex justify-content-center input mb-5 pb-5">
