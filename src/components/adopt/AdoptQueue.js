6a89c5df7661da72fcc78d7c83e872aeb5a01694
aniledev <elinamcgill@gmail.com>
2021-03-12 14:55:51 -0500

    FEAT: add bare bones HTTP request to adopt queue

--- src/components/adopt/AdoptQueue.js
+++ src/components/adopt/AdoptQueue.js
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
 import STORE from "../../dummy-store";
+import PORT_URL from "../../config";
 
 export default class AdoptQueue extends Component {
   componentDidMount() {
-    // fetch the adopters from the server using GET /api/people
-    // use chained promises to update the stat
-    // use this.setState to update this.state.adopters with response data
+    /* when the component is mounted, immediately preform a GET request to /api/people 
+       to receive adopters in the queue*/
+
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(`${PORT_URL}/api/people`, requestOptions)
+      // use chained promises to receive the data from the server
+      .then((response) => response.json())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+    // use this.setState to update this.state.people with response data
   }
 
-  // add a timeout method that adopts a pet every 5 seconds, dequeue a person every 5s
+  // add a timeout method that adopts a pet every 5 seconds, dequeue a person every 5sec
 
   render() {
     function queue() {
