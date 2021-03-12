8c8b4b2188f21dc53330442d4b7c6454f0cc24c4
aniledev <elinamcgill@gmail.com>
2021-03-12 14:50:44 -0500

    CHORE: add psuedocode for GET /api/people request for receiving people in the queue

--- src/components/adopt/AdoptQueue.js
+++ src/components/adopt/AdoptQueue.js
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 import STORE from "../../dummy-store";
 
 export default class AdoptQueue extends Component {
+  componentDidMount() {
+    // fetch the adopters from the server using GET /api/people
+    // use chained promises to update the stat
+    // use this.setState to update this.state.adopters with response data
+  }
+
+  // add a timeout method that adopts a pet every 5 seconds, dequeue a person every 5s
+
   render() {
     function queue() {
       const adopters = STORE.people;
