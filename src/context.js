17ad1b96c8157203f86182c2e0938aa5fcf2adb0
aniledev <elinamcgill@gmail.com>
2021-03-16 02:12:31 -0400

    CHORE: remove unnecessary syntax

---
+++ src/context.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// use createContext to initialize context for use in application
+
+export default React.createContext({
+  // empty update functions
+  handleAdoptClick: () => {},
+});
