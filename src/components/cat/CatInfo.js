0b24778a7dbd4d50a76c19bb41f8646fc9dcb165
aniledev <elinamcgill@gmail.com>
2021-03-26 16:31:17 -0400

    REFRACTOR: update cat and dog info using the information in state

--- src/components/cat/CatInfo.js
+++ src/components/cat/CatInfo.js
@@ -2,13 +2,20 @@ import React, { Component } from "react";
 
 export default class CatInfo extends Component {
   render() {
+    const firstCat = this.props.cats[0];
+    const name = firstCat["name"];
+    const age = firstCat["age"];
+    const sex = firstCat["gender"];
+    const breed = firstCat["breed"];
+    const story = firstCat["story"];
+
     return (
       <div className="mt-3">
-        <p>Name: Steve French</p>
-        <p>Sex: Female</p>
-        <p>Age: 2</p>
-        <p>Breed: Bengal</p>
-        <p>Story: Thrown on the street</p>
+        <p>Name: {name}</p>
+        <p>Sex: {sex}</p>
+        <p>Age: {age}</p>
+        <p>Breed: {breed}</p>
+        <p>Story: {story}</p>
       </div>
     );
   }
--- src/components/dog/DogInfo.js
+++ src/components/dog/DogInfo.js
@@ -2,13 +2,20 @@ import React, { Component } from "react";
 
 export default class DogInfo extends Component {
   render() {
+    const firstDog = this.props.dogs[0];
+    const name = firstDog["name"];
+    const age = firstDog["age"];
+    const sex = firstDog["gender"];
+    const breed = firstDog["breed"];
+    const story = firstDog["story"];
+
     return (
       <div className="mt-3">
-        <p>Name: Zim</p>
-        <p>Sex: Male</p>
-        <p>Age: 3</p>
-        <p>Breed: Golden Retriever</p>
-        <p>Story: Owner passed away</p>
+        <p>Name: {name}</p>
+        <p>Sex: {sex}</p>
+        <p>Age: {age}</p>
+        <p>Breed: {breed}</p>
+        <p>Story: {story}</p>
       </div>
     );
   }
