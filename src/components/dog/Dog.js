c229864583865a32ddef48b39cfbf8a3c70f77ff
aniledev <elinamcgill@gmail.com>
2021-04-05 23:59:44 -0400

    FEAT: conditionally render adoption confirmation message when button clicked

--- src/components/dog/Dog.js
+++ src/components/dog/Dog.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import DogHeading from "./DogHeading";
 import DogImage from "./DogImage";
 import DogInfo from "./DogInfo";
+import AdoptionConfirmation from "../../AdoptionConfirmation";
 import config from "../../config";
 
 export default class Dog extends Component {
@@ -11,6 +12,7 @@ export default class Dog extends Component {
     this.state = {
       dogs: [],
       error: null,
+      clicked: false,
     };
     this.handleClick = this.handleClick.bind(this);
     this.handleGetDogs = this.handleGetDogs.bind(this);
@@ -50,16 +52,33 @@ export default class Dog extends Component {
   }
 
   handleClick() {
+    // update clicked state
+    this.handleClickState();
     // function needs to trigger a DELETE request using the server
     this.dequeueDog();
     // once the cat/dog is dequeued, it also needs to update the state with the new set of cats and dogs
     this.handleGetDogs();
-    alert("You're now a proud dog parent!");
+    setTimeout(() => {
+      this.setState({ clicked: false });
+    }, 3000);
   }
 
   // add a timeout method that adopts a dog every 5 seconds
   /* add a conditional rendering that will disable the adopt button if 
   it is not the users turn in the queue*/
+
+  handleClickState() {
+    this.setState({ clicked: true });
+    console.log("This.state.clicked === true");
+  }
+
+  confirmAdoption() {
+    const clicked = this.state.clicked;
+    if (clicked === true) {
+      console.log("state changed to true");
+      return "You're now a proud pet parent!";
+    }
+  }
 
   render() {
     const { dogs } = this.state;
@@ -68,14 +87,22 @@ export default class Dog extends Component {
       return <p>Loading Information</p>;
     }
 
+    const clicked = this.state.clicked;
+    const message = this.confirmAdoption();
+    let confirmationMessage;
+    if (clicked) {
+      confirmationMessage = <AdoptionConfirmation message={message} />;
+    } else {
+      confirmationMessage = <></>;
+    }
+
     return (
       <div className="col-md text-center">
         <DogHeading dogs={dogs} />
         <DogImage dogs={dogs} />
         <DogInfo dogs={dogs} />
         <div className="mb-5">
-          {/* add event handler to button to trigger DELETE /api/dogs dequeue 
-          a dog, this also will update the state*/}
+          {confirmationMessage}
           <button
             type="button"
             className="btn adopt-button"
