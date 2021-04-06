1ea74e4d16440f46adf173b3be4cc3ed56f54fc1
aniledev <elinamcgill@gmail.com>
2021-04-05 23:50:42 -0400

    TEST: update test file to reflect named imports

--- src/tests/Root.test.js
+++ src/tests/Root.test.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { render, screen } from "@testing-library/react";
 import Root from "../components/Root.js";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(
-    <BrowserRouter>
+    <Router>
       <Root />
-    </BrowserRouter>,
+    </Router>,
     div
   );
   ReactDOM.unmountComponentAtNode(div);
